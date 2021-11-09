30fea4ac8b31492bde52b9655a58d114232d1498
Vern Chao <vernchao91@gmail.com>
2021-11-09 11:30:24 -0800

    finished message error reducer

--- frontend/reducers/errors/errors_reducer.js
+++ frontend/reducers/errors/errors_reducer.js
@@ -1,13 +1,15 @@
 import { combineReducers } from "redux";
 import sessionErrorsReducer from "./session_errors_reducer";
-import serverErrorsReducer from "./server_errors_reducer"
-import userErrorsReducer from "./user_errors_reducer"
-import channelErrorReducer from "./channel_errors_reducer"
+import serverErrorsReducer from "./server_errors_reducer";
+import userErrorsReducer from "./user_errors_reducer";
+import channelErrorReducer from "./channel_errors_reducer";
+import messageErrorReducer from "./message_errors_reducer"
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer,
   servers: serverErrorsReducer,
   channels: channelErrorReducer,
+  messages: messageErrorReducer,
   users: userErrorsReducer
 });
 
