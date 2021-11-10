fcd720c4e37c2f065beb4812b1a6144c9c9d42bc
Vern Chao <vernchao91@gmail.com>
2021-11-10 14:56:26 -0800

    redux-logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -15,11 +15,9 @@ const configureStore = (preloadedState = {}) =>
 
 
 // const middlewares = [thunk];
-
 // if (process.env.NODE_ENV !== "production") {
 //   middlewares.push(logger);
 // }
-
 // const configureStore = (preloadedState = {}) =>
 //   createStore(RootReducer, preloadedState, applyMiddleware(...middlewares));
 
