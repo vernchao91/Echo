b68753be3a38830c223f3d585ff98476f2f1f924
vernchao91 <vernchao91@gmail.com>
2021-11-30 23:47:49 -0800

    using second push logger code

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,24 +1,24 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/root_reducer";
-// import logger from "redux-logger";
-
-const middlewares = [thunk];
-
-if (process.env.NODE_ENV !== "production") {
-  const { logger } = require("redux-logger");
-  middlewares.push(logger);
-}
-
-const configureStore = (preloadedState = {}) =>(
-  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares)));
+import logger from "redux-logger";
 
 // const middlewares = [thunk];
+
 // if (process.env.NODE_ENV !== "production") {
+//   const { logger } = require("redux-logger");
 //   middlewares.push(logger);
 // }
+
 // const configureStore = (preloadedState = {}) =>
-//   createStore(RootReducer, preloadedState, applyMiddleware(...middlewares));
+//   createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
+
+const middlewares = [thunk];
+if (process.env.NODE_ENV !== "production") {
+  middlewares.push(logger);
+}
+const configureStore = (preloadedState = {}) =>
+  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
 
 // const configureStore = (preloadedState = {}) => {
 //   return (
