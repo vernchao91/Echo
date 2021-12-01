a3bb4c196ae27d749de04bfacc1491fb984e1aa9
vernchao91 <vernchao91@gmail.com>
2021-11-30 23:29:57 -0800

    added paranthesis

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -10,8 +10,8 @@ if (process.env.NODE_ENV !== "production") {
   middlewares.push(logger);
 }
 
-const configureStore = (preloadedState = {}) =>
-  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
+const configureStore = (preloadedState = {}) =>(
+  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares)));
 
 // const middlewares = [thunk];
 // if (process.env.NODE_ENV !== "production") {
